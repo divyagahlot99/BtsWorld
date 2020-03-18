cd196666c2a3d4026367fb5e5a5aa7fa33b7e353
DivyaGahlot <gahlotdivya1019@gmail.com>
2020-03-18 16:04:38 +0530

    committed

--- app/src/main/java/com/example/btsworld/MainActivity.java
+++ app/src/main/java/com/example/btsworld/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.btsworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.*;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
+        Button b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+
+        });
     }
 }
+
+
